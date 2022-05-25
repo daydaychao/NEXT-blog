b8feb3487cd4016b7a9a6ebb48664315d12b6680
day.ch <day.ch.tcg@gmail.com>
2022-05-25 17:28:16 +0800

    feat: 🎸 nextjs typescript blog init

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,28 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home({ allPostsData }) {
+import axios from 'axios'
+
+type propsType = {
+  allPostsData: [
+    {
+      date: string
+      title: string
+      id: string
+    }
+  ]
+}
+
+export default function Home({ allPostsData }: propsType) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Nextjs typescript</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -38,7 +47,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
